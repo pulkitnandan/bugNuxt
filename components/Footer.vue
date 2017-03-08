1416cc00ea5c587c087acf7ef4166d5dc23efc33
Rohit Nandan <pulkitnandan@gmail.com>
2017-03-08 19:44:32 +0530

    BUG POC

---
+++ components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>
+    Visit our website for more documentation : <a href="https://nuxtjs.org" target="_blank">nuxtjs.org</a>
+  </footer>
+</template>
+<script>
+  import { get, urls } from '~plugins/axios'
+
+  export default {
+    async data () {
+      const bugData = await get(urls.getMe())
+      return {
+        bug : bugData
+      }
+    },
+    methods: {
+      onNavClick () {
+        this.isOpen = !this.isOpen
+      }
+    }
+  }
+</script>
